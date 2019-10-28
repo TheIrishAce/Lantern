429687316d74ae8b54fdf6dad2276f0546d8d701
Max Delaney <maxdelaney16@gmail.com>
2019-10-28 16:27:11 +0000

    Added new database connection string. Insertion will now work correctly.

--- lib/php/config.php
+++ lib/php/config.php
@@ -1,6 +1,6 @@
 <?php
 // variable con, is used to connect to the DB. 3 arguments IP, username, password.
-$con = mysqli_connect('sql2.freemysqlhosting.net','sql2305939','kV9*wE9*');
+$con = mysqli_connect('zwgaqwfn759tj79r.chr7pe7iynqr.eu-west-1.rds.amazonaws.com','pgucq3rzgknilr0v','s6kw31seob0c22s3');
 
 //Local host con.
 //$con = mysqli_connect('127.0.0.1','root','');
@@ -11,8 +11,8 @@ if (!$con)
   echo 'Not connected to server, check database connection!';
 }
 
-// error checking to see if the DB at the IP is selectable (access is available) to database named in 2nd argument .
-if(!mysqli_select_db($con,'sql2305939'))
+// error checking to see if the DB at the IP is selectable (access is available) to database schema named in 2nd argument .
+if(!mysqli_select_db($con,'udnln55v3aodpcyp'))
 {
   echo 'Database not selected or selectable!';
 }
