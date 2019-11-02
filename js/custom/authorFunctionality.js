d760d6780a497746f1dff4c353a601ab154957b7
Max Delaney <maxdelaney16@gmail.com>
2019-11-02 20:34:45 +0000

    Added the ability to Save & Load user stories.

--- js/custom/authorFunctionality.js
+++ js/custom/authorFunctionality.js
@@ -1,4 +1,5 @@
 function insertStory() {
+
   var userStory =$("#writingArea").val();
   //var pass=$("#userPassword").val();
   //alert(userStory);
@@ -29,9 +30,52 @@ function insertStory() {
     });
   }
 
-  else
-  {
-    alert("please fill in all details !");
-  }
+  //else
+  //{
+  //  alert("please fill in all details !");
+  //}
+  return false;
+}
+
+function fetchStory() {
+  var userStory = "ABC";
+  //var pass=$("#userPassword").val();
+  //alert(userStory);
+  //alert(pass);
+  //if (name!="" && pass!="") {
+  //if (userStory!="") {
+  //alert("Retriving Information");
+
+    $.ajax({
+      url: 'lib/php/interactStory.php',
+      type: 'POST',
+      async: false,
+      data: {
+        load: 1,
+        storyType:userStory
+      },
+      success: function(response){
+        //console.log(response);
+        var obj = JSON.parse(response);
+        //console.log(obj);
+        if (obj.exit =="success") {
+          //document.getElementById('writingArea').innerHTML = "";
+          document.getElementById('writingArea').innerHTML = obj.UserStory.UserStory;
+          //alert(obj.UserStory.UserStory);
+          //$("writingArea").html(obj);
+          alert ("Story Loaded");
+        }
+        else
+        {
+          alert ("Error, story doesn't exist or couldn't be loaded.");
+        }
+      }
+    });
+  //}
+
+  //else
+  //{
+  //  alert("please fill in all details !");
+  //}
   return false;
 }
--- lib/php/interactStory.php
+++ lib/php/interactStory.php
@@ -1,5 +1,6 @@
 <?php
   session_start();
+
   if (isset($_POST['save']))
   {
     include 'config.php';
@@ -18,4 +19,56 @@
       }
     }
   }
+  
+  elseif (isset($_POST['load']))
+  {
+    include 'config.php';
+    $AuthorId = 'Bobbie_Rossie';
+    $TextArea = $conn->real_escape_string($_POST["storyType"]);
+    if(!empty($TextArea)){
+      $TextArea = filter_var($TextArea, FILTER_SANITIZE_STRING);
+
+
+      $query = "SELECT UserStory FROM user_story WHERE UserStoryAuthor='$AuthorId'";
+      $result = $conn->query($query);
+      $data = $result->fetch_assoc();
+
+
+      //echo $result;
+      if ($result->num_rows > 0)
+      {
+        $storyArray['exit'] = 'success';
+        $storyArray['UserStory'] = $data;
+        echo json_encode($storyArray);
+        //exit('success');
+	    }
+
+      else
+      {
+        exit('failed');
+      }
+      exit();
+    }
+  }
+
+  /*
+  elseif (isset($_POST['delete']))
+  {
+    include 'config.php';
+    $TextArea = $conn->real_escape_string($_POST["storyType"]);
+    if(!empty($TextArea)){
+      $TextArea = filter_var($TextArea, FILTER_SANITIZE_STRING);
+      $data = $conn->query("INSERT INTO user_story (UserStory) VALUES ('$TextArea')");
+      if ($data)
+      {
+          exit('success');
+	    }
+
+      else
+      {
+          exit('failed');
+      }
+    }
+  }
+  */
 ?>
