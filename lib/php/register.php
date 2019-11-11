489f23c57081f6249f440ba6f30a455ab2202d97
Dean Kelly <18100287@student.ncirl.ie>
2019-11-11 12:10:24 +0000

    updated register

--- lib/php/register.php
+++ lib/php/register.php
@@ -1,8 +1,33 @@
 <?php
     include 'config.php';
-    $AccountUsername = $_POST['AccountUsername'];
-    $AccountPassword = $_POST['AccountPassword'];
-   // $EventDescription = $_POST['eventDescription'];
+    if(isset($_POST['AccountPassword'])){
+      $AccountUsername = $_POST['AccountUsername'];
+      $AccountPassword = $_POST['AccountPassword']; 
+      $ConfirmPassword = $_POST['ConfirmPassword'];
+
+      if($AccountPassword==$ConfirmPassword){
+          $query = "selet * from site_account WHERE AccountUsername='$AccountUsername '";
+          $query_run =mysql_query($con,$query);
+
+          if(mysql_num_rows($query_run)>0){
+            echo 'User already exists';
+          }
+          {
+            $sql = "INSERT INTO site_account (AccountUsername,AccountPassword) VALUES ('$AccountUsername', '$AccountPassword')";
+
+            if(!mysqli_query($conn,$sql))
+            {
+              echo 'Error, Data not inserted';
+            }
+            else
+            {
+              echo 'Data inserted';
+            }
+          }
+      }
+
+    }
+    
 
     if(!empty($AccountUsername) && !empty($AccountPassword) ){
 
@@ -10,16 +35,9 @@
       $AccountPassword = filter_var($AccountPassword, FILTER_SANITIZE_STRING);
       
 
-      $sql = "INSERT INTO site_account (AccountUsername,AccountPassword) VALUES ('$AccountUsername', '$AccountPassword')";
+      
 
-      if(!mysqli_query($conn,$sql))
-      {
-        echo 'Error, Data not inserted';
-      }
-      else
-      {
-        echo 'Data inserted';
-      }
+      
 
       //refresh the index page after 2 seconds.
   		header("refresh:2; url=../../index.php");
