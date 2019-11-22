38457316d06d2e1f6e8bd05902e9b0aee4281c16
Dean Kelly <18100287@student.ncirl.ie>
2019-11-22 12:08:33 +0000

    more edits on forgot

--- lib/php/includes/reset-request.inc.php
+++ lib/php/includes/reset-request.inc.php
@@ -1,17 +1,19 @@
 <?php
+
+
  if (isset($_POST["reset-request-submit"])){
    $selector=bin2hex(random_bytes(8));
    $token=random_bytes(32);
 
-   $url="https://lanterngrape.herokuapp.com/create-new-password.php?selector=".$selector ."&validator"=.bin2hex($token);
+   $url="https://lanterngrape.herokuapp.com/create-new-password.php?selector=".$selector ."&validator=".bin2hex($token);
 
    $expires =date("U")+900;
 
-    require 'config.php';
+   require 'config.php';
 
-    $userEmail=$_POST["email"]
+    $userEmail=$_POST["email"];
 
-    $sql="DELETE FROM password_reset WHERE email_reset=?; ";
+    $sql="DELETE FROM password_reset WHERE email_reset=?";
     $stmt= mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt,$sql)){
       echo "There was an error!!!";
@@ -27,7 +29,7 @@
       echo "There was an error!!!";
       exit();
     }else{
-      $hashedToken=password_hash($token,PASSWORD_DEFAULT)
+      $hashedToken=password_hash($token,PASSWORD_DEFAULT);
       mysqli_stmt_bind_param($stmt,"ssss",$userEmail,$selector,$hashedToken,$expires);
       mysqli_stmt_execute($stmt);
     }
